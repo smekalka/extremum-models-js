140f2fc2dc96677a85b121761176320b2f891211
Mikhail Sokolov <mikhail.sokolov@smekalka.com>
2025-10-27 03:37:28 +0300

    Fixed package.json

--- index.js
+++ index.js
@@ -6,6 +6,6 @@
 
 'use strict';
 
-export * from "./lib/Model.js";
-export * from "./lib/ModelSet.js";
-export * from "./lib/Namespace.js";
+export * from './lib/Model.js';
+export * from './lib/ModelSet.js';
+export * from './lib/Namespace.js';
