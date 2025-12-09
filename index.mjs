e926857d9e1e0fdb50189caee76bbba9351f39b8
Mikhail Sokolov <mikhail.sokolov@smekalka.com>
2025-12-09 07:47:41 +0300

    4.0.13

---
+++ index.mjs
@@ -0,0 +1,18 @@
+/*!
+ * Module: @extremum/models
+ * Copyright(c) Smekalka, 2017—2025
+ * License: ISC
+ */
+
+'use strict';
+
+//import { createRequire } from 'module';
+//const require = createRequire(import.meta.url);
+
+export { Namespace } from './lib/Namespace.mjs';
+export { System } from './lib/System.mjs';
+export { Model } from './lib/Model.mjs';
+export { Instance } from './lib/Instance.mjs';
+export { Descriptor } from './lib/Descriptor.mjs';
+export { Origin } from './lib/Origin.mjs';
+export { Collection } from './lib/Collection.mjs';
---
+++ index.test.js
@@ -0,0 +1,17 @@
+/*!
+ * Module: @extremum/models
+ * Test: All
+ * Copyright(c) Smekalka, 2017—2025
+ * License: ISC
+ */
+'use strict';
+
+import { } from './index.mjs';
+
+describe('A bigger test set of the whole module', () => {
+
+	test('Test 1', () => {
+
+	});
+
+})
